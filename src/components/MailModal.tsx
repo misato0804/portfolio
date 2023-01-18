e50467170e40ff51c595d1276346fbb8abb7a191
Misato Tanno <breitmeer.0804@gmail.com>
2023-01-17 19:38:25 -0800

    Changed file

--- src/components/MailModal.tsx
+++ src/components/MailModal.tsx
@@ -7,15 +7,16 @@ type modalProps = {
 
 const MailModal = ({showModal} : modalProps) => {
     return (
-        <div
-            className={`duration-300 ${showModal? "bg-[rgba(115,115,115,0.5)]": "invisible"} w-screen h-screen fixed flex justify-center items-center top-0 left-0`}>
-            <div className={`duration-300 ${showModal? "bg-[rgba(225,225,225,1.0)]": "invisible"} px-12 py-4 flex justify-center items-center`}>
-                <div className={`flex duration-300 ${showModal? "": "invisible"}`}>
-                    {/*<img src={icon} alt="checkmark" className={`inline-block w-6 duration-300 ${showModal? "": "opacity-0"}`}/>*/}
-                    <p className={`ml-4 text-black duration-300 ${showModal? "": "opacity-0"}`}>Thanks :)</p>
-                </div>
-            </div>
-        </div>
+        <></>
+        // <div
+        //     className={`duration-300 ${showModal? "bg-[rgba(115,115,115,0.5)]": "invisible"} w-screen h-screen fixed flex justify-center items-center top-0 left-0`}>
+        //     <div className={`duration-300 ${showModal? "bg-[rgba(225,225,225,1.0)]": "invisible"} px-12 py-4 flex justify-center items-center`}>
+        //         <div className={`flex duration-300 ${showModal? "": "invisible"}`}>
+        //             {/*<img src={icon} alt="checkmark" className={`inline-block w-6 duration-300 ${showModal? "": "opacity-0"}`}/>*/}
+        //             <p className={`ml-4 text-black duration-300 ${showModal? "": "opacity-0"}`}>Thanks :)</p>
+        //         </div>
+        //     </div>
+        // </div>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,39 @@ module.exports = {
       },
       opacity: {
         "75": ".75"
+      },
+      animation: {
+        "shake-horizontal": "shake-horizontal 0.8s cubic-bezier(0.455, 0.030, 0.515, 0.955) both",
+        "animationTexted": "animatedText 10s infinite"
+      },
+      keyframes: {
+        "shake-horizontal": {
+          "0%,to": {
+            transform: "translateX(0)"
+          },
+          "10%,30%,50%,70%": {
+            transform: "translateX(-5px)"
+          },
+          "20%,40%,60%": {
+            transform: "translateX(5px)"
+          },
+          "80%": {
+            transform: "translateX(3px)"
+          },
+          "90%": {
+            transform: "translateX(-3px)"
+          }
+        },
+        "animatedText": {
+          "0%, 100%": {
+            "background-size": "400% 400%",
+            "background-position:": "left center"
+          },
+          "50%": {
+            "background-size": "400% 400%",
+            "background-position": "right center"
+          }
+        }
       }
     }
   }
