708d90fc89fc4c005f8725dca477461c7db92326
Misato Tanno <breitmeer.0804@gmail.com>
2023-01-17 18:00:03 -0800

    Changed file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
 import Footer from "./components/Footer";
+import Nav from "./components/Nav";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
       <div className="App common">
         <h3 className="z-0 fixed text-9xl origin-bottom-left rotate-90 text-back-text top-40 left-0 font-extrabold">Hi,there</h3>
         < Router>
-          {/*< Nav />*/}
-          {/*<Routes>*/}
-          {/*  < Route path="/" element={<Home/>}/>*/}
-          {/*  < Route path="/about" element={<About/>}/>*/}
-          {/*  < Route path="/contact" element={<Contact/>}/>*/}
-          {/*</Routes>*/}
+          < Nav />
+          <Routes>
+            < Route path="/" element={<Home/>}/>
+            < Route path="/about" element={<About/>}/>
+            < Route path="/contact" element={<Contact/>}/>
+          </Routes>
           <Footer/>
         </Router>
       </div>
--- src/components/MailModal.tsx
+++ src/components/MailModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import icon from "../images/check.png"
+// import icon from "../images/check.png"
 
 type modalProps = {
     showModal: boolean
@@ -11,7 +11,7 @@ const MailModal = ({showModal} : modalProps) => {
             className={`duration-300 ${showModal? "bg-[rgba(115,115,115,0.5)]": "invisible"} w-screen h-screen fixed flex justify-center items-center top-0 left-0`}>
             <div className={`duration-300 ${showModal? "bg-[rgba(225,225,225,1.0)]": "invisible"} px-12 py-4 flex justify-center items-center`}>
                 <div className={`flex duration-300 ${showModal? "": "invisible"}`}>
-                    <img src={icon} alt="checkmark" className={`inline-block w-6 duration-300 ${showModal? "": "opacity-0"}`}/>
+                    {/*<img src={icon} alt="checkmark" className={`inline-block w-6 duration-300 ${showModal? "": "opacity-0"}`}/>*/}
                     <p className={`ml-4 text-black duration-300 ${showModal? "": "opacity-0"}`}>Thanks :)</p>
                 </div>
             </div>
